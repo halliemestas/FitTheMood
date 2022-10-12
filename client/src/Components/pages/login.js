11b47f9679521b1bcffe8a5cc21828e0f07593fd
ZacharyFeltman <zachary.feltman@gmail.com>
2022-10-12 14:18:30 -0600

    login changed

---
+++ client/src/Components/pages/login.js
@@ -0,0 +1,26 @@
+import React, { useState } from 'react';
+import { Form, Button, Alert } from 'react-boostrap';
+
+import { loginUser } from '../../utils/API';
+import Auth from '../../utils/auth';
+
+const LoginForm = () => {
+    const [userFormData, setUserFormData] = useState({ username: '', password: '' });
+    const [validated] = useState(false);
+    const [showAlert, setShowAlert] = useState(false);
+
+    const handleInputChange = (event) => {
+        const { name, value } = event.target;
+        setUserFormData({ ...userFormData, [name]: value });
+    };
+
+    const handleFormSubmit = async (event) => {
+        event.preventDefault();
+
+        const form = event.currentTarget;
+        if (form.checkValidity() === false) {
+            event.preventDefault();
+            event.stopPropagation();
+        }
+    }
+}
