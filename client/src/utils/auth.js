873c14d8afcac6dc3ecc2bac52dac8ada776321b
ZacharyFeltman <zachary.feltman@gmail.com>
2022-10-13 10:07:49 -0600

    fixed spelling errors

--- client/src/utils/auth.js
+++ client/src/utils/auth.js
@@ -13,7 +13,7 @@ class AuthService {
   isTokenExpired(token) {
     try {
       const decoded = decode(token);
-      if (decoded.exp < Dats.now() / 1000) {
+      if (decoded.exp < Date.now() / 1000) {
         return true;
       } else return false;
     } catch (error) {
