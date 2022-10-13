fd1517e588eb331c63b4518e543f4df57a859c0b
Kamary <gillespiekamary@gmail.com>
2022-10-13 10:13:00 -0600

    app.js has login component

--- client/src/App.js
+++ client/src/App.js
@@ -3,12 +3,14 @@
 import React from "react";
 import Login from "./Components/pages/login";
 import Mood from "./Components/pages/mood";
+import NavBar from "./Components/pages/navbar";
 
 function App() {
   return (
     <>
       <Mood />
       <Login />
+      <NavBar />
     </>
   );
 }
