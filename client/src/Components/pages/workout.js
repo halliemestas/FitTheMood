e604530c3da1a9bdef848eb2c9775fe13808a5bd
Anthony Santo <tonymsanto@gmail.com>
2022-10-13 13:09:53 -0600

    Deleted code that has been commented out

--- client/src/Components/pages/workout.js
+++ client/src/Components/pages/workout.js
@@ -219,67 +219,6 @@ function WorkoutForm() {
         </Container>
       </div>
     </>
-
-  //   <>
-  //     <Button style={{backgroundColor: "#99A799", border: "#99A799", backgroundColorHover: "#D3E4CD"}} className="m-3" onClick={handleShow}>
-  //       Add Workout!
-  //     </Button>
-
-  //     <Modal show={show} onHide={handleClose}>
-  //       <Modal.Header closeButton>
-  //       <Modal.Title>Workout Questionaire</Modal.Title>
-  //       </Modal.Header>
-  //       <Modal.Body></Modal.Body>
-
-  //     <Form>
-      
-  //     <Form.Group>
-  //       <Form.Label>How long did you workout for?</Form.Label>
-  //       <Col sm={0}>
-  //           <Form.Check
-  //             type="radio"
-  //             label="0-30 Minutes"
-  //             name="formHorizontalRadios"
-  //             id="formHorizontalRadios1"
-  //           />
-  //           <Form.Check
-  //             type="radio"
-  //             label="30-60 Minutes"
-  //             name="formHorizontalRadios"
-  //             id="formHorizontalRadios2"
-  //           />
-  //           <Form.Check
-  //             type="radio"
-  //             label="60-90 Minutes"
-  //             name="formHorizontalRadios"
-  //             id="formHorizontalRadios3"
-  //           />
-  //           <Form.Check
-  //             type="radio"
-  //             label="90+ Minutes"
-  //             name="formHorizontalRadios"
-  //             id="formHorizontalRadios3"
-  //           />
-  //         </Col>
-  //     </Form.Group>
-
-  //     <Form.Group className="mb-3" controlId="formBasicEmail">
-  //       <Form.Label>What type of workout did you do?</Form.Label>
-  //       <Form.Control type="email" placeholder="Enter workout type" as="textarea" rows={1} />
-  //       <Form.Text className="text-muted"></Form.Text>
-  //     </Form.Group>
-
-  //     <Form.Group className="mb-3" controlId="formBasicPassword">
-  //       <Form.Label>Summary of your workout.</Form.Label>
-  //       <Form.Control type="Summary" placeholder="Summary" as="textarea" rows={3} />
-  //     </Form.Group>
-
-  //     <Button variant="primary" type="submit">
-  //       Submit
-  //     </Button>
-  //   </Form>
-  //   </Modal>
-  // </>
   );
 }
 
