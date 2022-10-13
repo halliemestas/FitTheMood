d08aceca5569cd11bf9fa0a223eb1e5a6a3bc180
ZacharyFeltman <zachary.feltman@gmail.com>
2022-10-13 13:55:09 -0600

    added logout button for nav

--- client/src/Components/pages/navbar.js
+++ client/src/Components/pages/navbar.js
@@ -4,6 +4,7 @@ import { NavLink } from 'react-router-dom'
 export default function NavBar() {
   return (
     <nav>
+    <h1>Fit The Mood</h1>
       <ul>
         <li>
           <NavLink exact to="/home">Home</NavLink>
@@ -13,6 +14,9 @@ export default function NavBar() {
         </li>
         <li>
           <NavLink to="/mood">Mood</NavLink>
+        </li>
+        <li>
+          <NavLink to="/">Log Out</NavLink>
         </li>
       </ul>
     </nav>
